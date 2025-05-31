b56ed197952af1ce010fce31f8452af1b9152794
Léana 江 <leana.jiang+git@icloud.com>
2025-05-31 13:38:26 +0200

    Add ChhoeTaigi_KamJitian as source

--- generate.py
+++ generate.py
@@ -97,6 +97,18 @@ with open(os.path.join(DICTIONARY_PATH, "ChhoeTaigi_KauiokpooTaigiSutian.csv")) 
             if unicodedata.category(c) == 'Lo':
                 WORDSET.add(c)
 
+with open(os.path.join(DICTIONARY_PATH, "ChhoeTaigi_KamJitian.csv")) as csvfile:
+    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    header = reader.__next__()
+    header = { v: k for (k, v) in enumerate(header) }
+    for row in reader:
+        for c in (
+            row[header['HanLoTaibunPoj']]
+            + row[header['KaisoehHanLoPoj']]
+        ):
+            if unicodedata.category(c) == 'Lo':
+                WORDSET.add(c)
+
 with open(os.path.join(DICTIONARY_PATH, "ChhoeTaigi_TaioanPehoeKichhooGiku.csv")) as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar='"')
     header = reader.__next__()
